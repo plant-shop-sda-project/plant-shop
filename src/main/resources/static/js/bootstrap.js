e1f267fcbe95a8a76746eea98bbbc1b7f62f5ff4
ikharevich <ikharewicz@gmail.com>
2022-07-27 13:44:27 +0200

    2022-07-22: Add style to "Login page" and "Register page"

---
+++ src/main/resources/static/js/bootstrap.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('.form-outline').forEach((formOutline) => {
+    new mdb.Input(formOutline).init();
+});
+
+document.querySelectorAll('.form-outline').forEach((formOutline) => {
+    new mdb.Input(formOutline).update();
+});
